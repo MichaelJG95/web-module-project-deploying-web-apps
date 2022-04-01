d5cd9e024b339a140a17467104acb0b36863ecbe
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-01 02:27:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+
+function App(props) {
+  
+  return (
+    <div className="App">
+      <h2>vercel project</h2>
+    </div>
+  );
+}
+
+
+
+export default App;
